9929ee71ff87c5cbd28477ecc14b2569fa21b9b0
Sam Mear <sam.mear2@gmail.com>
2023-06-09 22:21:21 +0100

    API Call

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Sam Movie App
 
 React Chakra
-Builds on what I learnt after building SamTFLApp
 
-`npm start` Starts the development server.
+Builds on what I learnt after building SamTFLApp. This is the second time using React for me, SamTFLApp being the first.
 
-`npm run build` Bundles the app into static files for production.
+## To run
+
+1. Obtain a API key from OMDb
+
+2. Place your API key in my-movie-app/.env.local like so `REACT_APP_OMDB_KEY=your_api_key`
+
+3. `cd my-movie-app`
+
+* `npm start` Starts the development server.
+
+* `npm run build` Bundles the app into static files for production.
--- my-movie-app/src/MovieSearch.js
+++ my-movie-app/src/MovieSearch.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+  Card,
+    CardHeader,
     ChakraProvider,
+    Image,
     Text
   } from '@chakra-ui/react';
 
 
 export const MovieSearch = () => {
-  return <Text>The movie app!</Text>;
+  const [movieData, setMovieData] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const response = await fetch(`http://www.omdbapi.com/?t=joker&apikey=${process.env.REACT_APP_OMDB_KEY}`);
+        const data = await response.json();
+        setMovieData(data);
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
+    };
+
+    fetchMovieData();
+  }, []);
+
+  if (!movieData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Card>
+      <CardHeader>{movieData.Title}</CardHeader>
+      <Text>{movieData.Plot}</Text>
+      <Image src={movieData.Poster}></Image>
+      <h2>Debug info below</h2>
+      <Text>{movieData.Error}</Text>
+      <Text>Your API Key: {process.env.REACT_APP_OMDB_KEY}</Text>
+      <Text>The Request Made: {`http://www.omdbapi.com/?t=joker&apikey=${process.env.REACT_APP_OMDB_KEY}`}</Text>
+    </Card>
+  );
 };
