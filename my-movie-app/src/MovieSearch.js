e72e36dea01e6de9983f801ccae16f10cfb88d9b
Sam Mear <sam.mear2@gmail.com>
2023-06-11 16:07:49 +0100

    Comments to pick up later

--- my-movie-app/src/MovieSearch.js
+++ my-movie-app/src/MovieSearch.js
@@ -1,3 +1,27 @@
+/**
+ * 
+ * Autocomplete not sure how to do. Plan is to store the movies locally first, then 
+ * do the autocomplete on the locally stored data.
+ * If user typed: lord, and in local data the lord of the rings was there, it would show up.
+ * 
+ * For stored data:
+ * useEffect(() => {
+    const storedData = localStorage.getItem('movieData');
+    if (storedData) {
+      setMovieData(JSON.parse(storedData));
+    }
+  }, []);
+ * the useEffect hook is used to check if there is any previously stored movie data in localStorage. 
+ * If there is, it retrieves the data and sets it as the initial value for movieData. 
+ * This ensures that if the user has previously searched for a movie, the data will be loaded from localStorage
+ * 
+ * and to store it:
+ * localStorage.setItem('movieData', JSON.stringify(data));
+ * 
+ * Maybe each movie will be stored with a date, like a cache, so after 7 days it can be considered dead and would need to be updated.
+ * 
+ */
+
 import React, { useEffect, useState } from 'react';
 import {
   Card,
@@ -33,9 +57,8 @@ export const MovieSearch = () => {
   const fetchMovieData = async () => {
     try {
       const response = await fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=${process.env.REACT_APP_OMDB_KEY}`);
-      //search is s=
-      //then use search results to get id, then use i= for that
-      //t= is just by title.
+      // TODO : Needs to check if it successfully return an error before setting the search data.
+      // IT also needs remove the unnecessary results (games ect) before it sets the data.
       const data = await response.json();
       setSearchData(data);
     } catch (error) {
